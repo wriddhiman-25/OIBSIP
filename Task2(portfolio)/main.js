3dd5f93a5ac348e080b9892c1f04e25cd4b6a25e
Wriddhiman Dey <113209496+wriddhiman-25@users.noreply.github.com>
2023-10-27 13:18:02 +0530

    Add files via upload

---
+++ Task2(portfolio)/main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["Web Designer","Frontend Developer","Software Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
